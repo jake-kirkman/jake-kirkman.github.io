b293956b66a825af72bb6c1f7c003fbf3fb1de6b
Jake Kirkman <jake.kirkman@greengage.co>
2023-09-26 21:05:18 +0100

    Added some custom type definitions

---
+++ src/custom.d.ts
@@ -0,0 +1,16 @@
+/**
+ * This is the global type definition file for the project, meaning you can define
+ * imports that may not actually exist within here and it will understand it,
+ * the JS engine may complain though, so be careful there
+ */
+
+//Makes the typescript engine not care about image imports
+declare module "*.svg" {
+    const content: any;
+    export default content;
+}
+
+declare module "*.png" {
+    const content: any;
+    export default content;
+}
