bcd7227a566a33465284da4020b2e6e391fd3ff2
Jake Kirkman <jake.kirkman@greengage.co>
2023-10-14 16:35:46 +0100

    Pill initial commit

---
+++ src/components/pill.tsx
@@ -0,0 +1,50 @@
+/*=========================================================
+        Imports
+=========================================================*/
+
+import React from 'react';
+import Tag from '../models/enums/tag';
+
+/*=========================================================
+        Interfaces
+=========================================================*/
+
+export interface PillState {
+
+}
+
+export interface PillProps {
+    tag: Tag;
+}
+
+/*=========================================================
+        Component
+=========================================================*/
+
+export default class Pill extends React.Component<PillProps, PillState> {
+
+    /*=========================================================
+            Setup
+    =========================================================*/
+
+    constructor(pProps: PillProps) {
+        super(pProps);
+    }
+
+    /*=========================================================
+            Hooks
+    =========================================================*/
+
+    /*=========================================================
+            Render
+    =========================================================*/
+
+    render(): React.ReactNode {
+        return (
+            <span className="cursor-pointer rounded-pill border-2 border-primary border bg-primary-subtle fw-semibold px-2 py-1">
+                {this.props.tag}
+            </span>
+        )
+    }
+
+}
