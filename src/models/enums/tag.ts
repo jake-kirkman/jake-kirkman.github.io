ea4b51493f176b28ba3fc220802610e78c8fa90d
Jake Kirkman <checko9@hotmail.co.uk>
2023-10-14 16:36:17 +0100

    Various interfaces and enums

---
+++ src/models/enums/tag.ts
@@ -0,0 +1,21 @@
+enum Tag {
+    React       = 'React',
+    Javascript  = 'Javascript',
+    Typescript  = 'Typescript',
+    Apex        = 'Apex',
+    Salesforce  = 'Salesforce',
+    CSS         = 'CSS',
+    HTML        = 'HTML',
+    LWC         = 'LWC',
+    Visualforce = 'Visualforce',
+    Java        = 'Java',
+    VSCode      = 'VSCode',
+    Bootstrap   = 'Bootstrap',
+    AzureDevOps = 'Azure DevOps',
+    Wrike       = 'Wrike',
+    RESTApi     = 'REST APIs',
+    Agile       = 'Agile',
+    XML         = 'XML',
+    JSON        = 'JSON'
+}
+export default Tag;
