cb29c6cc5348b1a939be9ce4e9631e6bb374414b
Jake Kirkman <jake.kirkman@greengage.co>
2023-10-14 16:37:40 +0100

    Updated custom typescript defs

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -10,7 +10,17 @@ declare module "*.svg" {
     export default content;
 }
 
+declare module "*.jpg" {
+    const content: any;
+    export default content;
+}
+
 declare module "*.png" {
     const content: any;
+    export default content;
+}
+
+declare module "*.md" {
+    const content: string;
     export default content;
 }
