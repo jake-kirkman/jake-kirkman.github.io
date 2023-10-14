3f46d13db425324708caad84568d76ec27a5fbc4
Jake Kirkman <jake.kirkman@greengage.co>
2023-10-14 16:38:13 +0100

    Page Constants

---
+++ src/constants/pageConstants.ts
@@ -0,0 +1,40 @@
+import Page from "../models/interfaces/page";
+import MarkdownPage from "../pages/markdownPage";
+import HOME_MD_PATH from "./../content/home.md";
+import PERSONAL_PROJECTS_MD_PATH from "./../content/personalProjects.md";
+
+import PROFESSIONAL_PROJECTS from "../content/professionalProjects/_discovery.ts";
+import PERSONAL_PROJECTS from "../content/personalProjects/_discovery.ts";
+import ListPage from "../pages/listPage";
+
+const PAGE_DEFINITIONS: Readonly<Page>[] = [
+    Object.freeze({
+        Name: 'Home',
+        Path: '/',
+        Component: MarkdownPage,
+        Props: {
+            MarkdownFilePath: HOME_MD_PATH
+        }
+    }),
+    Object.freeze({
+        Name: 'Professional Projects',
+        Path: '/professional-projects',
+        Component: ListPage,
+        Props: {
+            PageTitle: 'Professional Projects',
+            PageDescription: 'Here you\'ll find a collection of various projects I\'ve overseen, planned, designed, developed and maintained. Feel free to utilise the filters below if you\'re looking for a particular skillset',
+            Items: PROFESSIONAL_PROJECTS
+        }
+    }),
+    Object.freeze({
+        Name: 'Personal Projects',
+        Path: '/personal-projects',
+        Component: ListPage,
+        Props: {
+            PageTitle: 'Personal Projects',
+            PageDescription: 'Here you\'ll find a collection of various personal projects I\'ve undertaken',
+            Items: PERSONAL_PROJECTS
+        }
+    }),
+];
+export default PAGE_DEFINITIONS;
