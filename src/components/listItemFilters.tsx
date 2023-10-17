11a21c801b6236f47ca41993b67bd84eaa9b8229
Jake Kirkman <jake.kirkman@greengage.co>
2023-10-17 14:45:33 +0100

    Additional work on ListItemFilters WIP

--- src/components/listItemFilters.tsx
+++ src/components/listItemFilters.tsx
@@ -6,7 +6,8 @@ import React from 'react';
 import Tag from '../models/enums/tag';
 import SortDirection from '../models/enums/sortDirection';
 import ListItem from '../models/interfaces/listItem';
-import ICN_FILTERS from '../assets/icons/icn_filters.svg';
+import ICN_ARROW_UP from '../assets/icons/icn_arrow_up.svg';
+import ICN_ARROW_DOWN from '../assets/icons/icn_arrow_down.svg';
 import Pill from './pill';
 
 /*=========================================================
@@ -43,6 +44,8 @@ export default class ListItemFilters extends React.Component<ListItemFiltersProp
     =========================================================*/
 
     onSearchChange = (pEvent: React.ChangeEvent<HTMLInputElement>) => {this.handleSearchChange(pEvent);}
+    onComboboxChange = (pEvent: React.ChangeEvent<HTMLSelectElement>) => {this.handleComboboxChange(pEvent);}
+    onSortClick = (pEvent: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {this.handleSortButtonClick(pEvent);}
 
     /*=========================================================
             Setup
@@ -52,7 +55,7 @@ export default class ListItemFilters extends React.Component<ListItemFiltersProp
         super(pProps);
         this.state = {
             Filters: {
-                SelectedTags: [],
+                SelectedTags: Object.values(Tag),
                 SortDirection: SortDirection.Descending,
                 SortField: 'Date'
             }
@@ -75,16 +78,32 @@ export default class ListItemFilters extends React.Component<ListItemFiltersProp
         }
     }
 
+    updateFilters(pFilters: Partial<Filters>) {
+        this.setState({
+            Filters: Object.assign(this.state.Filters, pFilters)
+        }, this.fireFilterChangeEvent);
+    }
+
     /*=========================================================
             Handlers
     =========================================================*/
 
     handleSearchChange(pEvent: React.ChangeEvent<HTMLInputElement>) {
-        this.setState({
-            Filters: Object.assign(this.state.Filters, {
-                SearchTerm: pEvent.target.value
-            })
-        }, this.fireFilterChangeEvent);
+        this.updateFilters({
+            SearchTerm: pEvent.target.value
+        })
+    }
+
+    handleComboboxChange(pEvent: React.ChangeEvent<HTMLSelectElement>) {
+        this.updateFilters({
+            SortField: pEvent.target.value as keyof ListItem
+        });
+    }
+
+    handleSortButtonClick(pEvent: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        this.updateFilters({
+            SortDirection: this.state.Filters.SortDirection == SortDirection.Ascending ? SortDirection.Descending : SortDirection.Ascending
+        });
     }
 
     /*=========================================================
@@ -94,25 +113,35 @@ export default class ListItemFilters extends React.Component<ListItemFiltersProp
     render(): React.ReactNode {
         return (
             <div className="w-100 d-flex flex-column gap-2">
-                <div className="d-flex w-100 flex-row gap-2">
-                    <div>
-                        <input
-                            type="search"
-                            className="form-control"
-                            id="searchInput"
-                            placeholder="Search..."
-                            onChange={this.onSearchChange}
-                        />
-                    </div>
-                    <div>
-                        <select className="form-select">
+                {/* <h1>Filters</h1> */}
+                <div>
+                    <input
+                        type="search"
+                        className="form-control"
+                        id="searchInput"
+                        placeholder="Search for projects..."
+                        value={this.state.Filters.SearchTerm}
+                        onChange={this.onSearchChange}
+                    />
+                </div>
+                <div className="d-flex flex-row gap-2 align-items-end">
+                    <div className="w-100">
+                        <label htmlFor="orderByInput" className="form-label">Order By</label>
+                        <select 
+                            className="form-select" 
+                            id="orderByInput"
+                            value={this.state.Filters.SortField}
+                            onChange={this.onComboboxChange}
+                        >
                             <option value="Date">Date</option>
                             <option value="Label">Title</option>
                         </select>
                     </div>
+                    <button className="btn btn-outline-primary h-fit-content" style={{aspectRatio: "1/1"}} onClick={this.onSortClick}>
+                        <img width={20} src={this.state.Filters.SortDirection == SortDirection.Ascending ? ICN_ARROW_UP : ICN_ARROW_DOWN}/>
+                    </button>
                 </div>
                 <div className="w-100">
-                    <h3>Filters</h3>
                     <div className="w-100 d-flex flex-wrap flex-row">
                         {
                             Object.values(Tag).map(
