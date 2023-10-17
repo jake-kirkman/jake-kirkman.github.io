694185cf255a818424c5c64412a3ec03d33a0f0c
Jake Kirkman <jake.kirkman@greengage.co>
2023-10-17 17:25:03 +0100

    Added Onboarding portal to Professional Projects

--- src/content/professionalProjects/_discovery.ts
+++ src/content/professionalProjects/_discovery.ts
@@ -3,6 +3,7 @@ import ListItem from './../../models/interfaces/listItem';
 import Tag from '../../models/enums/tag';
 //Markdown Imports
 import TRANSACTION_APPROVALS from './transactionApprovals.md';
+import ONBOARDING_PORTAL from './onboardingPortal.md';
 import ACTIVITIES_2 from './activities2.md';
 
 const PROFESSIONAL_PROJECTS: ListItem[] = [
@@ -11,6 +12,12 @@ const PROFESSIONAL_PROJECTS: ListItem[] = [
         Tags: [Tag.React, Tag.CSS, Tag.HTML, Tag.Typescript],
         Date: '2023-07',
         MarkdownDataPath: TRANSACTION_APPROVALS
+    },
+    {
+        Label: 'Onboarding Portal',
+        Tags: [Tag.Salesforce, Tag.Apex, Tag.HTML, Tag.CSS, Tag.Javascript, Tag.LWC, Tag.RESTApi, Tag.JSON],
+        Date: '2022-12',
+        MarkdownDataPath: ONBOARDING_PORTAL
     },
     {
         Label: 'Activities 2.0',
