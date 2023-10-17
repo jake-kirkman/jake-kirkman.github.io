c2186ab34ae4ef32cb3c70caa17aaa821c43bc6c
Jake Kirkman <jake.kirkman@greengage.co>
2023-10-17 17:24:03 +0100

    Removed pointer cursor

--- src/components/pill.tsx
+++ src/components/pill.tsx
@@ -40,8 +40,9 @@ export default class Pill extends React.Component<PillProps, PillState> {
     =========================================================*/
 
     render(): React.ReactNode {
+        //Cursor Pointer needs to be enabled once filtering has been added
         return (
-            <span className="cursor-pointer rounded-pill border-2 border-primary border bg-primary-subtle fw-semibold px-2 py-1">
+            <span className="/*cursor-pointer*/ rounded-pill border-2 border-primary border bg-primary-subtle fw-semibold px-2 py-1">
                 {this.props.tag}
             </span>
         )
