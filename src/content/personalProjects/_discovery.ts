e06af58114bfc22940c50dc61f427b763397e883
Jake Kirkman <checko9@hotmail.co.uk>
2024-10-17 17:41:59 +0100

    Added custom label generator project

--- src/content/personalProjects/_discovery.ts
+++ src/content/personalProjects/_discovery.ts
@@ -3,9 +3,16 @@ import ListItem from './../../models/interfaces/listItem';
 import Tag from '../../models/enums/tag';
 //Markdown Imports
 import PORTFOLIO_SITE from './portfolioSite.md';
+import SCLG_VSCODE_EXTENSION from './salesforceCustomLabelGeneratorVSCodeExtension.md';
 import WRIKE_VSCODE_EXTENSION from './wrikeVscodeExtension.md';
 
 const PERSONAL_PROJECTS: ListItem[] = [
+    {
+        Label: 'SF Custom Label VSCode Extension',
+        Tags: [Tag.Typescript, Tag.VSCode],
+        Date: '2024-01',
+        MarkdownDataPath: SCLG_VSCODE_EXTENSION
+    },
     {
         Label: 'Portfolio Site',
         Tags: [Tag.React, Tag.CSS, Tag.HTML, Tag.Typescript],
